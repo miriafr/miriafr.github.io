e40ecd7de8e85624ae787ccf7ce4af45baa1fdf3
miriafr <miriaframov@gmail.com>
2024-10-28 14:00:25 -0400

    about me page design change

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ import meter1 from "../assets/img/meter1.svg"
 import meter2 from "../assets/img/meter2.svg"
 import meter3 from "../assets/img/meter3.svg"
 import colorSharp from "../assets/img/color-sharp.png"
-
+import aboutImg from "../assets/img/about-image.jpeg"
 export const AboutMe = () => {
     const responsive = {
         superLargeDesktop: {
@@ -28,38 +28,39 @@ export const AboutMe = () => {
     };
 
     return (
-        <section className="skill" id="skills">
+        <section className="about" id="skills">
             <Container>
-                <Row>
-                    <Col>
-                    <div className="skill-bx">
-                        <h2>
-                            Skills
-                        </h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>UX/UI</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Graphic Design</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                    </Col>
+                <Row className="align-items-center about-bx">
+                        <Col sm={12} xs={12} md={6} xl={4}>
+                            <img className="about-img" src={aboutImg}></img>
+                        </Col>
+                        <Col xs={12} sm={12} md={6} xl={8}>
+                            <h2>
+                                About Me
+                            </h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            {/* <Carousel responsive={responsive} infinite={true} className="about-slider">
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Web Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter2} alt="Image" />
+                                    <h5>Design</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter3} alt="Image" />
+                                    <h5>UX/UI</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={meter1} alt="Image" />
+                                    <h5>Graphic Design</h5>
+                                </div>
+                            </Carousel> */}
+                        </Col>
                 </Row>
             </Container>
-            <img className="background-image-left" src={colorSharp}></img>
+            {/* <img className="background-image-left" src={colorSharp}></img> */}
         </section>
     )
 }
